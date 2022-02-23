638e79f0199419580bae2dd9a10ac4ce58961984
Lenovo <s4ntalovs@yandex.ru>
2022-02-23 16:31:52 +0800

    4.2. Результат работы метода [#232622]

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,18 +1,13 @@
 package ru.job4j.calculator;
 
-public class Calculator {
+public class MathFunc {
 
+    public static int func1(int x) {
+        int y = 50 * 49 + 1;
+        return y;
+    }
     public static void main(String[] args) {
-        int one = 1;
-        int two = 2;
-        int six = 6;
-        int four = 4;
-        int five = 5;
-        int sixDivTwo = six / two;
-        int fiveMinTwo = five - two;
-        int fourTimeTwo = four * two;
-        System.out.println(sixDivTwo);
-        System.out.println(fiveMinTwo);
-        System.out.println(fourTimeTwo);
+        int result1 = MathFunc.func1(100);
+        System.out.println(result1);
     }
 }

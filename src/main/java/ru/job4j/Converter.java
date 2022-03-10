ceb5ea32f0296b8a9ae904d7ec9fafff6b1ed9a4
Сергей Санта <98329226+s4ntalovs@users.noreply.github.com>
2022-03-10 20:47:07 +0800

    Update Converter.java

--- src/main/java/ru/job4j/Converter.java
+++ src/main/java/ru/job4j/Converter.java
@@ -13,6 +13,7 @@ public class Converter {
     public static void main(String[] args) {
         float Dollar = Converter.rubleToDollar(140);
         float euro = Converter.rubleToEuro(140);
-        System.out.println("140 rubles are " + euro + " euro.");    }
+        System.out.println("140 rubles are " + euro + " euro.");    
         System.out.println("140 rubles are " + Dollar + " Dollar.");
+    }
 }
